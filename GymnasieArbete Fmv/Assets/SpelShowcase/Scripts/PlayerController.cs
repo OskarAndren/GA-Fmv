4f8752272486110068a2281e0646e641c98e1296
OskarAndren <123725447+OskarAndren@users.noreply.github.com>
2025-05-08 10:46:37 +0200

    build

--- GymnasieArbete Fmv/Assets/SpelShowcase/Scripts/PlayerController.cs
+++ GymnasieArbete Fmv/Assets/SpelShowcase/Scripts/PlayerController.cs
@@ -4,7 +4,6 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class PlayerController : MonoBehaviour
 {

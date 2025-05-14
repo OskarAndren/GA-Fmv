079a1c9f66630b41b5d510e017bd9c3421921a83
OskarAndren <123725447+OskarAndren@users.noreply.github.com>
2025-05-14 11:25:43 +0200

    La till Movement,Animations,Enemies spel test

---
+++ GymnasieArbete Fmv/Assets/SpelShowcase/Scripts/AnimationsSpel.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimationsSpel : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator ani;
+    BoxCollider2D feet_Col;
+    CapsuleCollider2D body_Col;
+
+    Vector2 moveInput;
+
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 10f;
+    private float maxYVeolcity = -20f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        feet_Col = GetComponent<BoxCollider2D>();
+        body_Col = GetComponent<CapsuleCollider2D>();
+    }
+
+    bool deadTrigger = false;
+    void Update()
+    {
+        if (rb.velocity.y < maxYVeolcity) rb.velocity = new Vector2(rb.velocity.x, maxYVeolcity);
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    void OnJump()
+    {
+        if (feet_Col.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            rb.velocity += new Vector2(0f, jumpSpeed);
+    }
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
+
+        if (moveInput.x != 0)
+        {
+            ani.SetBool("isRunning", true);
+            transform.localScale = new Vector2(Mathf.Sign(moveInput.x), transform.localScale.y);
+        }
+        else
+        {
+            ani.SetBool("isRunning", false);
+        }
+    }
+}
